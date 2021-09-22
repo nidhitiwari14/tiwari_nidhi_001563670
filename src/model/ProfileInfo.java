1ab42cf6002f00f927348bf9be97f7b949f5516a
Nidhi Tiwari <tiwari.n@northeastern.edu>
2021-09-22 13:18:56 -0400

    Create Main Panel with Buttons

---
+++ src/model/ProfileInfo.java
@@ -0,0 +1,179 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+import java. util. Date;
+
+/**
+ *
+ * @author nidhitiwari
+ */
+public class ProfileInfo {
+    
+    private String name;
+    private String geographicData;
+    private Date dateOfBirth;
+    private int phoneNumber;
+    private String faxNumber;
+    private String emailAddress;
+    private String ssnNumber;
+    private String medicalRecordNumber;
+    private String healthPlanBeneficiaryNumber;
+    private String[] bankAccountNums;
+    private String licenseNum;
+    private String licensePlate;
+    private String licensePlateNum;
+    private String deviceSerialNum;
+    private String linkedinProfile;
+    private String internetProtocolAddress;
+    private long fingerPrint;
+    private long photograph; 
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGeographicData() {
+        return geographicData;
+    }
+
+    public void setGeographicData(String geographicData) {
+        this.geographicData = geographicData;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFaxNumber() {
+        return faxNumber;
+    }
+
+    public void setFaxNumber(String faxNumber) {
+        this.faxNumber = faxNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getSsnNumber() {
+        return ssnNumber;
+    }
+
+    public void setSsnNumber(String ssnNumber) {
+        this.ssnNumber = ssnNumber;
+    }
+
+    public String getMedicalRecordNumber() {
+        return medicalRecordNumber;
+    }
+
+    public void setMedicalRecordNumber(String medicalRecordNumber) {
+        this.medicalRecordNumber = medicalRecordNumber;
+    }
+
+    public String getHealthPlanBeneficiaryNumber() {
+        return healthPlanBeneficiaryNumber;
+    }
+
+    public void setHealthPlanBeneficiaryNumber(String healthPlanBeneficiaryNumber) {
+        this.healthPlanBeneficiaryNumber = healthPlanBeneficiaryNumber;
+    }
+
+    public String[] getBankAccountNums() {
+        return bankAccountNums;
+    }
+
+    public void setBankAccountNums(String[] bankAccountNums) {
+        this.bankAccountNums = bankAccountNums;
+    }
+
+    public String getLicenseNum() {
+        return licenseNum;
+    }
+
+    public void setLicenseNum(String licenseNum) {
+        this.licenseNum = licenseNum;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public String getLicensePlateNum() {
+        return licensePlateNum;
+    }
+
+    public void setLicensePlateNum(String licensePlateNum) {
+        this.licensePlateNum = licensePlateNum;
+    }
+
+    public String getDeviceSerialNum() {
+        return deviceSerialNum;
+    }
+
+    public void setDeviceSerialNum(String deviceSerialNum) {
+        this.deviceSerialNum = deviceSerialNum;
+    }
+
+    public String getLinkedinProfile() {
+        return linkedinProfile;
+    }
+
+    public void setLinkedinProfile(String linkedinProfile) {
+        this.linkedinProfile = linkedinProfile;
+    }
+
+    public String getInternetProtocolAddress() {
+        return internetProtocolAddress;
+    }
+
+    public void setInternetProtocolAddress(String internetProtocolAddress) {
+        this.internetProtocolAddress = internetProtocolAddress;
+    }
+
+    public long getFingerPrint() {
+        return fingerPrint;
+    }
+
+    public void setFingerPrint(long fingerPrint) {
+        this.fingerPrint = fingerPrint;
+    }
+
+    public long getPhotograph() {
+        return photograph;
+    }
+
+    public void setPhotograph(long photograph) {
+        this.photograph = photograph;
+    }
+    
+    
+}
